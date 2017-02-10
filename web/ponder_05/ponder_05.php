5356a95cd16b8cd89dc5100186509cd04c958225
Jonathan <jester1570@gmail.com>
2017-02-09 19:08:41 -0700

    stuffs

--- web/ponder_05/ponder_05.php
+++ web/ponder_05/ponder_05.php
@@ -17,38 +17,59 @@ Some parts of the php is not what I created. It is taken from the instructor fil
     <body>
         <div class="containerrow">
             <div id="side">
-                <p>This is stuff on the left side of the page</p>
-                
+                <div id="containercol">
+                    <p>This is profile information for(Type 1 for Jonathan and 2 for Seth):</p>
+                    <input type="text" name="selection" method="GET">
+                    
+                    <?php
+                    
+                        $choice = $_GET['selection'];
+                        // In this example, for simplicity, the query is executed
+                        // right here and the data echoed out as we iterate the query.
+                        // You could imagine that in a more involved application, we
+                        // would likely query the database in a completely separate file / function
+                        // and build a list of objects that held the components of each
+                        // scripture. Then, here on the page, we could simply call that 
+                        // function, and iterate through the list that was returned and
+                        // print each component.
+                        // First, prepare the statement
+                        // Notice that we avoid using "SELECT *" here. This is considered
+                        // good practice so we don't inadvertently bring back data we don't
+                        // want, especially if the database changes later.
+                        $statement = $db->prepare("SELECT fname, lname, bio FROM profile WHERE id=$choice");
+                        $statement->execute();
+                        // Go through each result
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                        {
+                            // The variable "row" now holds the complete record for that
+                            // row, and we can access the different values based on their
+                            // name
+                            echo '<p>' . $row['fname'] '</p>';
+                            echo '<p>' . $row['lname'] '</p>';
+                            echo '<p>' . $row['bio'] '</p>';
+                        }
+                        ?>
+                </div>
             </div>
             <div id="standard">
                 <div class="containercol">
                     <div id="standardtext">
                         <?php
-// In this example, for simplicity, the query is executed
-// right here and the data echoed out as we iterate the query.
-// You could imagine that in a more involved application, we
-// would likely query the database in a completely separate file / function
-// and build a list of objects that held the components of each
-// scripture. Then, here on the page, we could simply call that 
-// function, and iterate through the list that was returned and
-// print each component.
-// First, prepare the statement
-// Notice that we avoid using "SELECT *" here. This is considered
-// good practice so we don't inadvertently bring back data we don't
-// want, especially if the database changes later.
-$statement = $db->prepare("SELECT communication, player_id FROM conversation");
-$statement->execute();
-// Go through each result
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	echo '<p>';
-	echo $row['communication'];
-	echo '</p>';
-}
-?>
+                        
+                        $statement = $db->prepare("SELECT communication, player_id FROM conversation");
+                        $statement->execute();
+                        // Go through each result
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                        {
+                            // The variable "row" now holds the complete record for that
+                            // row, and we can access the different values based on their
+                            // name
+                            echo '<p>';
+                            echo $row['communication'];
+                            echo '</p>';
+                        }
+                        ?>
+                        
               <!--      <p id="text_color">
                     Admin 4:51:29PM: This is where the conversation text will appear. I think one problem I will run into is that this should actually be a text file but for now it is just stuff in the database.
                 </p>
