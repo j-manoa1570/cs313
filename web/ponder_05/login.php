6252a0fabb41a7eb9fa6b264e171aa545a408531
Jonathan <jester1570@gmail.com>
2017-02-09 22:39:51 -0700

    stuffs

--- web/ponder_05/login.php
+++ web/ponder_05/login.php
@@ -8,6 +8,7 @@ session_start();
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="icon" href="http://img02.deviantart.net/5f38/i/2003/40/6/3/welcome_to_the_machine.jpg">
         <title>Welcome to Machine!</title>
     </head>
     <body>
