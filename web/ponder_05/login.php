86573c89c936fc81bf26cb6de9d816c268609519
Jonathan <jester1570@gmail.com>
2017-02-10 14:08:53 -0700

    stuffs

--- web/ponder_05/login.php
+++ web/ponder_05/login.php
@@ -6,7 +6,7 @@ session_start();
 
 <!-- Login page in order to access user account -->
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <link rel="icon" href="http://img02.deviantart.net/5f38/i/2003/40/6/3/welcome_to_the_machine.jpg">
         <title>Welcome to Machine!</title>
@@ -14,14 +14,14 @@ session_start();
     <body>
         <div>
             <p>Welcome to Machine! Your one stop for increased productivity!</p>
-            <p>Username:
+            <form action="machine.php" method="post"><p>Username:
             <input type="text" name="username">
                 <br/>
                 Password:
-                
             <input type="text" name="password">
             <br/>
-                <input type="submit"></p>
+                <input type="submit" value="submit"></p>
+            </form>
         </div>
     </body>
 </html>
