e24413aaeec68628ca904a57084a46379923a985
Jonathan <jester1570@gmail.com>
2017-02-10 17:55:22 -0700

    stuffs

--- web/ponder_05/machine.php
+++ web/ponder_05/machine.php
@@ -32,10 +32,10 @@ The general format of the php to postgresql is taken from the instructor's solut
                     $username = 'jester1570';
                     $password = 'Jmonnao1.';
                     
-                    $user_id = $db->prepare("SELECT id FROM player WHERE username=$username");
+                    $user_id = $db->prepare("SELECT id FROM player WHERE username=$username AND password=$password");
                     $user_id->execute();
-                    $choice = $choice['id'];
-                    $statement = $db->prepare("SELECT fname, lname, bio FROM profile WHERE player_id=$choice");
+                    //$choice = $choice['id'];
+                    $statement = $db->prepare("SELECT fname, lname, bio FROM profile WHERE player_id=$user_id");
                     $statement->execute();
                         // Go through each result
                     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
