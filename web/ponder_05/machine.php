ad39e7c6e6c3d46c2f9507d3b70bd0e08ba579d1
Jonathan <jester1570@gmail.com>
2017-02-11 02:19:06 -0700

    stuffs

--- web/ponder_05/machine.php
+++ web/ponder_05/machine.php
@@ -8,7 +8,7 @@ $db = get_db();
 
 
 <!--
-The general format of the php to postgresql is taken from the instructor's solution from Week 04 Team Activity. I have modified it to fit my application so that my tables would function.
+Originally the PHP that was used for this page was the php that was found in the instructor solution in Week 05 Team Activity but was changed once it was found that it was not sufficient for my needs. Received help on writing PHP from Seth Childers who received help from another student taking a database class using MySQL.
 -->
 <!DOCTYPE html>
 <html lang="en">
@@ -25,17 +25,37 @@ The general format of the php to postgresql is taken from the instructor's solut
             <div id="side">
                 <div id="containercol">
                     <?php
-                        
+                    
+                    /**************************************************************************
+                     * This PHP does a couple of things. 
+                     * 1) Grabs user login information from login.php
+                     * 2) Prepares to access database
+                     * 3) Fetches player id using the username and password as criteria
+                     * 4) Fetches player profile information using player id as fk
+                     * 5) Outputs the player's profile information 
+                     * More detail is below in each of the 5 sections
+                     *************************************************************************/
+                    
+                    // 1) Takes the form information from login.php (username and password that was entered in) 
+                    //    and sets it to variables to be used for pinging the database.
                     $username = $_POST['username'];
                     $password = $_POST['password'];
                     
+                    // 2) Preparing to access the database by declaring what columns are being accessed from
+                    //    what table under what conditions. Variables are bound so that we can access data
+                    //    from database.
                     $user_id = $db->prepare('SELECT id FROM player WHERE username= :username AND password= :password');
                     $user_id->bindValue(':username', $username);
                     $user_id->bindvalue(':password', $password);
+                    
+                    // 3) The SQL command is executed, data is fetched, fetched data is assigned to a php
+                    //    variable, and SQL database connection is closed.
                     $user_id->execute();
                     $row = $user_id->fetch(PDO::FETCH_ASSOC);
-                                        
                     $user_id->closeCursor();
+
+                    // 4) This is a combination of steps 2 and 3 but this time to the profile table
+                    //    so we can retreive data on the player's profile.
                     $id = $row['id'];
                     $user_profile = $db->prepare('SELECT fname, lname, location, email, bio, title, phone FROM profile WHERE player_id= :id');
                     $user_profile->bindValue(':id', $id);
@@ -43,6 +63,7 @@ The general format of the php to postgresql is taken from the instructor's solut
                     $row_profile = $user_profile->fetch(PDO::FETCH_ASSOC);
                     $user_profile->closeCursor();
                     
+                    // 5) All of the retrieved data is outputted to the screen for the player.
                     echo '<p><strong>' . 'Name:</strong> ' . $row_profile['fname'] . ' ' . $row_profile['lname'] . '</p>';
                     echo '<p><strong>' . 'Title:</strong> ' . $row_profile['title'] . '</p>';
                     echo '<p><strong>' . 'Phone:</strong> ' . $row_profile['phone'] . '</p>';
@@ -58,19 +79,45 @@ The general format of the php to postgresql is taken from the instructor's solut
                 <div id="standard">
                     <div id="standardtext">
                         <?php
-                        
-//                        $statement = $db->prepare("SELECT communication, player_id FROM conversation");
-//                        $statement->execute();
-                        // Go through each result
-                        /*while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-                        {
-                            // The variable "row" now holds the complete record for that
-                            // row, and we can access the different values based on their
-                            // name
-                            echo '<p>';
-                            echo $row['communication'];
-                            echo '</p>';
-                        }*/
+                        /**************************************************************************
+                        * This PHP does a couple of things. 
+                        * 1) Grabs user login information from login.php
+                        * 2) Prepares to access database
+                        * 3) Fetches player id using the username and password as criteria
+                        * 4) Fetches player profile information using player id as fk
+                        * 5) Outputs the player's profile information 
+                        * More detail is below in each of the 5 sections
+                        *************************************************************************/
+                    
+                        // 1) Takes the form information from login.php (username and password that was entered in) 
+                        //    and sets it to variables to be used for pinging the database.
+                        $username = $_POST['username'];
+                        $password = $_POST['password'];
+                    
+                        // 2) Preparing to access the database by declaring what columns are being accessed from
+                        //    what table under what conditions. Variables are bound so that we can access data
+                        //    from database.
+                        $user_id = $db->prepare('SELECT id FROM player WHERE username= :username AND password= :password');
+                        $user_id->bindValue(':username', $username);
+                        $user_id->bindvalue(':password', $password);
+                    
+                        // 3) The SQL command is executed, data is fetched, fetched data is assigned to a php
+                        //    variable, and SQL database connection is closed.
+                        $user_id->execute();
+                        $row = $user_id->fetch(PDO::FETCH_ASSOC);
+                        $user_id->closeCursor();
+
+                        // 4) This is a combination of steps 2 and 3 but this time to the profile table
+                        //    so we can retreive data on the player's profile.
+                        $id = $row['id'];
+                        $user_profile = $db->prepare('SELECT communication FROM conversation WHERE player1_id= :id OR play2_id = :id');
+                        $user_profile->bindValue(':id', $id);
+                        $user_profile->execute();
+                        $row_conversation = $user_profile->fetch(PDO::FETCH_ASSOC);
+                        $user_profile->closeCursor();
+                       
+                        // 5) All of the retrieved data is outputted to the screen for the player.
+                        echo '<p>' . $row_conversation['communication'] . '</p>';
                         ?>
                     </div>
                     <br>
