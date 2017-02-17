c63ac47f7a1f8bfdcd2e83bee6a65bb8fb5ba7e9
Jonathan <jester1570@gmail.com>
2017-02-16 19:45:46 -0700

    stuffs

--- web/ponder_05/account_setup.php
+++ web/ponder_05/account_setup.php
@@ -65,7 +65,55 @@
     </body>
 </html>
 
+
 <?php
+/**********************************************************
+* File: createAccount.php
+* Author: Br. Burton
+* 
+* Description: Accepts a new username and password on the
+*	POST variable, and creates it in the DB.
+*
+* The user is then redirected to the signIn.php page.
+*
+***********************************************************/
+// If you have an earlier version of PHP (earlier than 5.5)
+// You need to download and include password.php.
+//require("password.php");
+// get the data from the POST
+$username = $_POST['new_user'];
+$password = $_POST['new_pass'];
+if (!isset($username) || $username == ""
+	|| !isset($password) || $password == "")
+{
+	header("Location: account_setup.php");
+	die(); // we always include a die after redirects.
+}
+// Let's not allow HTML in our usernames. It would be best to also detect this before
+// submitting the form and preven the submission.
+$username = htmlspecialchars($username);
+// Get the hashed password.
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+// Connect to the database
+require("heroku_access.php");
+$db = get_db();
+$query = 'INSERT INTO player (username, password) VALUES(:username, :password)';
+$statement = $db->prepare($query);
+$statement->bindValue(':username', $username);
+// **********************************************
+// NOTICE: We are submitting the hashed password!
+// **********************************************
+$statement->bindValue(':password', $hashedPassword);
+$statement->execute();
+// finally, redirect them to the sign in page
+header("Location: login.html");
+die(); // we always include a die after redirects. In this case, there would be no
+       // harm if the user got the rest of the page, because there is nothing else
+       // but in general, there could be things after here that we don't want them
+       // to see.
+?>
+<?php
+/**
 $password = $_POST['new_pass'];
 $username = $_POST['new_user'];
 // if username or password are empty, then redirect back to signup page
@@ -94,4 +142,7 @@ try {
 }
 header("Location: login.html");
 die();
+
+
+*/
 ?>
