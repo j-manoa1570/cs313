66b6369dcf55f85fbd8f70c44d923b01262ddbde
Jonathan <jester1570@gmail.com>
2017-02-20 18:20:08 -0700

    stuffs

--- web/ponder_05/account_creation.php
+++ web/ponder_05/account_creation.php
@@ -1,25 +1,25 @@
 <?php
 
 $password = $_POST['new_pass'];
-$user = $_POST['new_user'];
+$username = $_POST['new_user'];
 
-if (!isset($user) || $user == "" 
+if (!isset($username) || $username == "" 
     || !isset($password) || $password == "")
 {
     header("Location: account_setup.php");
     die();
 }
 
-$user = htmlspecialchars($user);
+$username = htmlspecialchars($username);
 
-$hashed = password_hash($password, PASSWORD_DEFAULT);
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 require("heroku_access.php");
 $db = get_db();
-
-$new_account = $db->prepare('INSERT INTO player(username, password) VALUES(:username, :hashed)');
-$new_account->bindValue(':username', $user);
-$new_account->bindValue(':hashed', $hashed);
+$query = 'INSERT INTO player(username, password) VALUES(:username, :password)';
+$new_account = $db->prepare($query);
+$new_account->bindValue(':username', $username);
+$new_account->bindValue(':password', $hashedPassword);
 $new_account->execute();
 
 header("Location: login.php");
