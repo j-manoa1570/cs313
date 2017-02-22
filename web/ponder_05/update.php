ec971a5ecf10d3388fcb89ce8cdc75f410799a31
Jonathan <jester1570@gmail.com>
2017-02-22 08:26:56 -0700

    stuffs

--- web/ponder_05/update.php
+++ web/ponder_05/update.php
@@ -10,16 +10,15 @@ $email = $_POST['new_email'];
 $bio = $_POST['new_bio'];
 
 
-if (/*!isset($fname) && $fname == "" && !isset($lname) && $lname == "" && */!isset($title) && $title == "" /*&& !isset($email) && $email == "" && !isset($bio) && $bio == ""*/)
+if (!isset($fname) && $fname == "" && !isset($lname) && $lname == "" && !isset($title) && $title == "" && !isset($email) && $email == "" && !isset($bio) && $bio == "")
 {
     echo "<script type='text/javascript'>alert('$message');</script>";
     header("Location: machine.php");
     die();
 }
 else {
-    /*if (isset($fname) && $fname != "")
+    if (isset($fname) && $fname != "")
     {
-        $fname = htmlspecialchars($fname);
         $query = 'INSERT INTO profile(fname) VALUES(:fname) WHERE profile_id = 1';
         $new = $db->prepare($query);
         $new->bindValue(':fname', $fname);
@@ -28,7 +27,6 @@ else {
 
     if (isset($lname) && $lname != "")
     {
-        $lname = htmlspecialchars($lname);
         $query = 'INSERT INTO profile(lname) VALUES(:lname) WHERE profile_id = 1';
         $new = $db->prepare($query);
         $new->bindValue(':lname', $lname);
@@ -39,7 +37,6 @@ else {
     {
         if (isset($title) && $title != "")
         {
-           // $title = htmlspecialchars($title);
             $query = 'update profile set title= :title where player_id = 1';
             $new = $db->prepare($query);
             $new->bindValue(':title', $title);
@@ -54,10 +51,8 @@ catch (Exception $ex)
 	die();
 }
 
-    /*
     if (isset($email) && $email != "")
     {
-        $email = htmlspecialchars($email);
         $query = 'INSERT INTO profile(email) VALUES(:email) WHERE profile_id = 1';
         $new = $db->prepare($query);
         $new->bindValue(':email', $email);
@@ -66,12 +61,11 @@ catch (Exception $ex)
 
     if (isset($bio) && $bio != "")
     {
-        $bio = htmlspecialchars($bio);
         $query = 'INSERT INTO profile(bio) VALUES(:bio) WHERE profile_id = 1';
         $new = $db->prepare($query);
         $new->bindValue(':bio', $bio);
         $new->execute();
-    }*/
+    }
 }
 
 header("Location: machine.php");
