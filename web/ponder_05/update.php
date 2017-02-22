e82577aaf4aae5ae0fc22b2fb237980bcf46e479
Jonathan <jester1570@gmail.com>
2017-02-21 22:21:07 -0700

    stuffs

--- web/ponder_05/update.php
+++ web/ponder_05/update.php
@@ -11,68 +11,79 @@ $email = $_POST['new_email'];
 $bio = $_POST['new_bio'];
 $phone = $_POST['new_phone'];
 
-if (isset($fname) && $fname != "")
+
+if (!isset($fname) && $fname == "" && !isset($lname) && $lname == "" && !isset($title) && $title == "" && !isset($location) && $location == "" && !isset($email) && $email == "" && !isset($bio) && $bio == "" && !isset($phone) && $phone == "");
 {
-    $fname = htmlspecialchars($fname);
-    $query = 'INSERT INTO profile(fname) VALUES(:fname)';
-    $new = $db->prepare($query);
-    $new->bindValue(':fname', $fname);
-    $new->execute();
+    echo "<script type='text/javascript'>alert('$message');</script>";
+    header("Location: machine.php");
+    die();
+}
+else {
+    if (isset($fname) && $fname != "")
+    {
+        $fname = htmlspecialchars($fname);
+        $query = 'INSERT INTO profile(fname) VALUES(:fname)';
+        $new = $db->prepare($query);
+        $new->bindValue(':fname', $fname);
+        $new->execute();
+    }
+
+    if (isset($lname) && $lname != "")
+    {
+        $lname = htmlspecialchars($lname);
+        $query = 'INSERT INTO profile(lname) VALUES(:lname)';
+        $new = $db->prepare($query);
+        $new->bindValue(':lname', $lname);
+        $new->execute();
+    }
+
+    if (isset($title) && $title != "")
+    {
+        $title = htmlspecialchars($title);
+        $query = 'INSERT INTO profile(title) VALUES(:title)';
+        $new = $db->prepare($query);
+        $new->bindValue(':title', $title);
+        $new->execute();
+    }
+
+    if (isset($location) && $location != "")
+    {
+        $location = htmlspecialchars($location);
+        $query = 'INSERT INTO profile(location) VALUES(:location)';
+        $new = $db->prepare($query);
+        $new->bindValue(':location', $location);
+        $new->execute();
+    }
+
+    if (isset($email) && $email != "")
+    {
+        $email = htmlspecialchars($email);
+        $query = 'INSERT INTO profile(email) VALUES(:email)';
+        $new = $db->prepare($query);
+        $new->bindValue(':email', $email);
+        $new->execute();
+    }
+
+    if (isset($bio) && $bio != "")
+    {
+        $bio = htmlspecialchars($bio);
+        $query = 'INSERT INTO profile(bio) VALUES(:bio)';
+        $new = $db->prepare($query);
+        $new->bindValue(':bio', $bio);
+        $new->execute();
+    }
+
+    if (isset($phone) && $phone != "")
+    {
+        $phone = htmlspecialchars($phone);
+        $query = 'INSERT INTO profile(phone) VALUES(:phone)';
+        $new = $db->prepare($query);
+        $new->bindValue(':phone', $phone);
+        $new->execute();
+    }
 }
 
-if (isset($lname) && $lname != "")
-{
-    $lname = htmlspecialchars($lname);
-    $query = 'INSERT INTO profile(lname) VALUES(:lname)';
-    $new = $db->prepare($query);
-    $new->bindValue(':lname', $lname);
-    $new->execute();
-}
-
-if (isset($title) && $title != "")
-{
-    $title = htmlspecialchars($title);
-    $query = 'INSERT INTO profile(title) VALUES(:title)';
-    $new = $db->prepare($query);
-    $new->bindValue(':title', $title);
-    $new->execute();
-}
-
-if (isset($location) && $location != "")
-{
-    $location = htmlspecialchars($location);
-    $query = 'INSERT INTO profile(location) VALUES(:location)';
-    $new = $db->prepare($query);
-    $new->bindValue(':location', $location);
-    $new->execute();
-}
-
-if (isset($email) && $email != "")
-{
-    $email = htmlspecialchars($email);
-    $query = 'INSERT INTO profile(email) VALUES(:email)';
-    $new = $db->prepare($query);
-    $new->bindValue(':email', $email);
-    $new->execute();
-}
-
-if (isset($bio) && $bio != "")
-{
-    $bio = htmlspecialchars($bio);
-    $query = 'INSERT INTO profile(bio) VALUES(:bio)';
-    $new = $db->prepare($query);
-    $new->bindValue(':bio', $bio);
-    $new->execute();
-}
-
-if (isset($phone) && $phone != "")
-{
-    $phone = htmlspecialchars($phone);
-    $query = 'INSERT INTO profile(phone) VALUES(:phone)';
-    $new = $db->prepare($query);
-    $new->bindValue(':phone', $phone);
-    $new->execute();
-}
-
+header("Locaion: machine.php");
+die();
 
 ?>
