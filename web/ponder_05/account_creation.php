68d510032a6153c87ab9f6f1f6bc1d3440734b85
Jonathan <jester1570@gmail.com>
2017-02-22 19:25:37 -0700

    stuffs

--- web/ponder_05/account_creation.php
+++ web/ponder_05/account_creation.php
@@ -21,7 +21,7 @@ $new_account = $db->prepare($query);
 $new_account->bindValue(':username', $username);
 $new_account->bindValue(':password', $hashedPassword);
 $new_account->execute();
-/*
+
 try{
 $query = 'SELECT id FROM player WHERE username = :username';
 $push_id = $db->prepare($query);
@@ -58,7 +58,7 @@ catch (Exception $ex)
     die();
 }
 
-*/
+
 
 header("Location: login.php");
 die();
