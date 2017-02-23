7fecb3e8e105c9d9e7d365d939cfe52715e38898
Jonathan <jester1570@gmail.com>
2017-02-22 18:09:13 -0700

    stuffs

--- web/ponder_05/login.php
+++ web/ponder_05/login.php
@@ -7,49 +7,59 @@ $badLogin = false;
 
 // First check to see if we have post variables, if not, just
 // continue on as always.
-try{
-if (isset($_POST['loginname']) && isset($_POST['loginpass']))
+try
 {
+    if (isset($_POST['loginname']) && isset($_POST['loginpass']))
+    {
 	// they have submitted a username and password for us to check
-	$username = $_POST['loginname'];
-	$password = $_POST['loginpass'];
+        $username = $_POST['loginname'];
+        $password = $_POST['loginpass'];
     
 	// Connect to the DB
-	require("dbConnect.php");
-	$db = get_db();
-	$query = 'SELECT password FROM login WHERE username=:username';
-	$statement = $db->prepare($query);
-	$statement->bindValue(':username', $username);
-	$result = $statement->execute();
-	if ($result)
-	{
-		$row = $statement->fetch();
-		$hashedPasswordFromDB = $row['password'];
+        require("dbConnect.php");
+        $db = get_db();
+        $query = 'SELECT password FROM login WHERE username=:username';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $result = $statement->execute();
+        if ($result)
+        {
+            $row = $statement->fetch();
+            $hashedPasswordFromDB = $row['password'];
         
+            try{
 		// now check to see if the hashed password matches
-		if (password_verify($password, $hashedPasswordFromDB))
-		{
+                if (password_verify($password, $hashedPasswordFromDB))
+                {
 			// password was correct, put the user on the session, and redirect to home
-			$_SESSION['username'] = $username;
-			header("Location: home.php");
-			die(); // we always include a die after redirects.
-		}
-		else
-		{
-			$badLogin = true;
-		}
-	}
-	else
-	{
-		$badLogin = true;
-	}
-}
+                    $_SESSION['username'] = $username;
+                    header("Location: home.php");
+                    die(); // we always include a die after redirects.
+                }
+                else
+                {
+                    $badLogin = true;
+                }
+            }
+            catch (Exception $ex)
+            {
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+                echo "Error with 'password_verify()' on line 32 Details: $ex";
+                die();
+            }
+        }
+        else
+        {
+            $badLogin = true;
+        }
+    }
 }
 catch (Exception $ex)
 {
 	// Please be aware that you don't want to output the Exception message in
 	// a production environment
-	echo "Error with 'fname' Details: $ex";
+	echo "Error with if statement on line 12 Details: $ex";
 	die();
 }
 // If we get to this point without having redirected, then it means they
