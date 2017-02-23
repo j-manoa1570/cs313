3b5f8519b404f76f70fc292961b6f99a4e20c70c
Jonathan <jester1570@gmail.com>
2017-02-22 19:12:36 -0700

    stuffs

--- web/ponder_05/account_creation.php
+++ web/ponder_05/account_creation.php
@@ -23,20 +23,33 @@ $new_account->bindValue(':username', $username);
 $new_account->bindValue(':password', $hashedPassword);
 $new_account->execute();
 
+try{
 $query = 'SELECT id FROM player WHERE username = :username';
 $push_id = $db->prepare($query);
 $push_id->bindValue(':username', $username);
 $push->execute();
 $row = $push_id->fetch(PDO::FETCH_ASSOC);
 $push_id->closeCursor();
+}
+catch (EXCEPTION $ex)
+{
+    echo "ERROR: Could not receive id from database (This is found at line 30). Details: $ex";
+}
 
 $id = $row['id'];
 $_SESSION['id'] = $id;
 
+try {
 $query = 'INSERT INTO profile(player_id) VALUES(:id)';
 $profile_id = $db->prepare($query);
 $profile_id->bindValue(':id', $id);
 $profile_id->execute();
+}
+catch (EXCEPTION $ex)
+{
+    echo "ERROR: Could not create new id in profile table. Details: $ex";
+}
+
 
 
 header("Location: login.php");
